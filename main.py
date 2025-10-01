3c92d64673fa39534e0cb592dd6aeef31a1b4ff3
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-10-01 12:28:03 -0400

    move methods to separate functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# from tabulate import tabulate
+from tabulate import tabulate
+from typing import Self
 # create method for creating objects of items
     # every item should have name, amount, availability (based on amount), cost(optional), item source
     # should each item hold individual objects or just have an amount attribute
@@ -8,7 +9,7 @@
 # you should be able to edit the item's attributes
 
 class Item: 
-    Inventory: dict = dict()
+    Inventory: dict[str, Self] = dict()
     def __init__(self, name: str, amount: float) -> None:
         self.name: str = name
         self.amount: float = amount
@@ -24,28 +25,58 @@ def createNewItem() -> Item | None:
     else:
         print("item already exists!")
 
+def deleteItem() -> None:
+    dItem: str = input("enter name of item to delete: ")
+    check: str = input(f"are you sure you want to delete {dItem}? This action is permanent and cannot be undone. (y/n): ").lower()
+    if dItem in Item.Inventory:
+        if check == "y":
+            del Item.Inventory[dItem]
+        else:
+            print("delete aborted.")
+    else:
+        print(f"{dItem} does not exist. Try viewing the inventory to see all items.")
+
+def checkOutItem() -> None:
+    cItem: str = input("which item are you checking out?: ")
+    cAmount: float = float(input("amount to check out: "))
+    if cItem in Item.Inventory:
+        item: Item = Item.Inventory[cItem] # use "item" as reference to item in inventory
+        if item.amount >= cAmount and item.amount > 0:
+            item.amount = item.amount - cAmount
+            print(f"successfully checked out {cAmount} of {cItem}.")
+            if item.amount == 0:
+                item.availability = False # check for updated availability when checking out item 
+        else:
+            print(f"not enough {cItem} to check out. there are only {item.amount} left. checkout failed.")
+    else:
+        print(f"{cItem} does not exist!")
+
+def viewInventory() -> None:
+    table: list = list()
+    for key, item in Item.Inventory.items():
+        table.append([key, item.amount, ("yes" if item.availability  else "no")])
+    print(tabulate(table, headers=['Name', 'Amount', 'Available?']))
 
 # CLI loop
-loop = ""
+loop = True
 for n in range(50): print("-",sep=' ', end='', flush=True)
+print("\nWelcome to the inventory system.\n")
 while loop:
-    selection: str = input("\nwelcome to the inventory system.\n"\
-                        "What would you like to do?\n"\
+    for n in range(50): print("-",sep=' ', end='', flush=True)
+    selection: str = input("\nWhat would you like to do?\n"\
                         "(N)ew item\n"\
                         "(D)elete item\n"\
+                        "(C)heck out items\n"\
                         "(V)iew inventory\n"\
                         "(E)xit\n"\
-                        ).lower()
+                        ).lower() # get user selection, accept any case
     if selection == "n" or selection == "new":
         createNewItem() # create new item
     elif selection == "d" or selection == "delete":
-        dItem = input("enter name of item to delete: ")
-        if dItem in Item.Inventory:
-            del Item.Inventory[dItem]
+        deleteItem() # delete item function
+    elif selection == "c" or selection == "check":
+        checkOutItem() # make call to checkout function
     elif selection == "v" or selection == "view":
-        table: list = list()
-        for i in Item.Inventory:
-            table.append([i.name, i.amount, ("yes" if i.availability  else "no")])
-        # print(tabulate(table, headers=['Name', 'Amount', 'Available?']))
+        viewInventory()
     elif selection == "e" or selection == "exit":
         loop = False
