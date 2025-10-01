12e87e8fb833951abc8a7a5588bf59a0907d0278
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-10-01 10:09:30 -0400

    change to dictionary usage. add type hints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tabulate import tabulate
+# from tabulate import tabulate
 # create method for creating objects of items
     # every item should have name, amount, availability (based on amount), cost(optional), item source
     # should each item hold individual objects or just have an amount attribute
@@ -7,29 +7,29 @@ from tabulate import tabulate
 # you should be able to check out a custom amount of items, not to exceed the amount available
 # you should be able to edit the item's attributes
 
-Inventory = set()
-
 class Item: 
-    def __init__(self, name, amount):
+    Inventory: dict = dict()
+    def __init__(self, name: str, amount: float) -> None:
         self.name: str = name
         self.amount: float = amount
         self.availability: bool = True if amount > 0 else False
-        Inventory.add(self)
+        Item.Inventory[self.name] = self
 
 
-def newItem() -> Item:
+def createNewItem() -> Item | None:
     name = input("item name: ")
     amount = float(input("Item amount: "))
-    return Item(name, amount)
-
-
+    if "%s" % (name) not in Item.Inventory:
+        return Item(name, amount)
+    else:
+        print("item already exists!")
 
 
 # CLI loop
 loop = ""
 for n in range(50): print("-",sep=' ', end='', flush=True)
-while loop is not "Exit":
-    selection = input("\nwelcome to the inventory system.\n"\
+while loop:
+    selection: str = input("\nwelcome to the inventory system.\n"\
                         "What would you like to do?\n"\
                         "(N)ew item\n"\
                         "(D)elete item\n"\
@@ -37,12 +37,15 @@ while loop is not "Exit":
                         "(E)xit\n"\
                         ).lower()
     if selection == "n" or selection == "new":
-        item = newItem()
+        createNewItem() # create new item
+    elif selection == "d" or selection == "delete":
+        dItem = input("enter name of item to delete: ")
+        if dItem in Item.Inventory:
+            del Item.Inventory[dItem]
     elif selection == "v" or selection == "view":
-        table = []
-        for i in Inventory:
+        table: list = list()
+        for i in Item.Inventory:
             table.append([i.name, i.amount, ("yes" if i.availability  else "no")])
-        print(tabulate(table, headers=['Name', 'Amount', 'Available?']))
-            # print(tabulate([[i.name, i.amount, ("yes" if i.availability  else "no")]], headers=['Name', 'Amount', 'Available?']))
+        # print(tabulate(table, headers=['Name', 'Amount', 'Available?']))
     elif selection == "e" or selection == "exit":
-        loop = "Exit"
+        loop = False
