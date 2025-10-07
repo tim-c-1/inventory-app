126a83c291fad5778630e544459e1275a7fe0874
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-10-07 14:35:48 -0400

    lots of changes to set up gui. set up adding and saving data

---
+++ gui.py
@@ -0,0 +1,170 @@
+from PyQt6.QtCore import QModelIndex, Qt, QAbstractTableModel
+from PyQt6.QtWidgets import QApplication, QHeaderView, QMainWindow, QVBoxLayout, QTabWidget, QLineEdit, QComboBox, QGridLayout, QLabel, QButtonGroup, QRadioButton, QWidget, QFileDialog, QPushButton, QCheckBox, QHBoxLayout, QTableView, QDialog, QDialogButtonBox
+from PyQt6.QtGui import QIcon
+import sys, os
+import pandas as pd
+from main import Item
+import main
+from typing import Any
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("testing")
+
+        # initialize data dictionary objects
+        main.Item.Inventory = main.loadInventory() 
+
+        self.central_widget = QWidget()
+        self.layout_1 = QVBoxLayout()
+
+        self.table = QTableView()
+        self.user_input_widget = UserInputWidget()
+        
+        # set up table model
+        data = self.readInventory()        
+        MainWindow.model = TableModel(data)
+        self.table.setModel(MainWindow.model)
+        
+        rowLabels: QHeaderView | None = self.table.verticalHeader()
+        if rowLabels:
+            rowLabels.setVisible(False)
+
+       
+        # build the structure
+        # self.setCentralWidget(self.user_input_widget)
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setLayout(self.layout_1)
+
+        self.layout_1.addWidget(self.table)
+        self.layout_1.addWidget(self.user_input_widget)
+        
+
+    def readInventory(self) -> pd.DataFrame:
+        inv: dict = main.loadInventory()
+        
+        return main.unpackInventory(inv)
+
+class TableModel(QAbstractTableModel):
+    def __init__(self, data: pd.DataFrame) -> None:
+        super().__init__()
+        self._data = data
+
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole) -> str | None:
+        if index.isValid():
+            if role == Qt.ItemDataRole.DisplayRole:
+                value = self._data.iloc[index.row(), index.column()]
+                return str(value)
+        
+    def rowCount(self, index: QModelIndex) -> int:
+        return self._data.shape[0]
+    
+    def columnCount(self, index: QModelIndex) -> int:
+        return self._data.shape[1]
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Orientation.Horizontal and role == Qt.ItemDataRole.DisplayRole:
+            return self._data.columns[col]
+
+    def resetData(self, new_data: pd.DataFrame) -> None:
+        self.beginResetModel()
+        print(f"data during resetData: {main.Item.Inventory}")
+        self._data = new_data
+        print(f"data after resetData: {main.Item.Inventory}")
+        self.endResetModel()
+
+class UserInputWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # make buttons
+        self.new_item_btn = QPushButton("New Item")
+        self.delete_item_btn = QPushButton("Delete Item")
+        self.edit_item_btn = QPushButton("Edit Item")
+        self.save_inv_btn = QPushButton("Save")
+
+        # add user hints
+        self.new_item_btn.setToolTip("create new inventory item")
+
+        # connect buttons
+        self.new_item_btn.pressed.connect(self.new_item_btn_pressed)
+        self.save_inv_btn.pressed.connect(self.save_inventory)
+
+        self.input_layout = QGridLayout()
+        
+        # input_layout.setContentsMargins(5,0,0,0)
+        
+        self.input_layout.addWidget(self.edit_item_btn, 1, 0)
+        self.input_layout.addWidget(self.new_item_btn, 0, 0)
+        self.input_layout.addWidget(self.delete_item_btn, 0, 1)
+        self.input_layout.addWidget(self.save_inv_btn, 1, 1)
+        
+        self.setLayout(self.input_layout)
+
+    def new_item_btn_pressed(self) -> None:
+        dlg = NewItemDialog()
+        dlg.exec()
+
+    def save_inventory(self) -> None:
+        main.saveInventory()
+        dlg = SaveInventoryMessage()
+        dlg.exec()
+
+class NewItemDialog(QDialog):
+    def __init__(self) -> None:
+            super().__init__()
+            self.setWindowTitle("create new item")
+
+            btn = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+            self.buttonBox = QDialogButtonBox(btn)
+            self.buttonBox.accepted.connect(self.accept)
+            self.buttonBox.rejected.connect(self.reject)
+
+            self.name = QLineEdit("Name")
+            self.amount = QLineEdit("Amount")
+            self.maxAmount = QLineEdit("Max Amount")
+            self.maxAmount.setToolTip("if different than current amount")
+            self.cost = QLineEdit("Cost")
+            self.source = QLineEdit("Item source")
+
+            formWidget = QGridLayout()
+            formWidget.addWidget(self.name)
+            formWidget.addWidget(self.amount)
+            formWidget.addWidget(self.maxAmount)
+            formWidget.addWidget(self.cost)
+            formWidget.addWidget(self.source)
+
+            layout = QVBoxLayout()
+            layout.addLayout(formWidget)
+            layout.addWidget(self.buttonBox)
+            self.setLayout(layout)
+    def accept(self) -> None:
+        try:
+            args = [self.name.text(), float(self.amount.text()), float(self.maxAmount.text()), float(self.cost.text()), self.source.text()]
+            if args:
+                main.createNewItem(*args)
+                inv = main.unpackInventory(main.Item.Inventory)
+                MainWindow.model.resetData(inv)
+            return super().accept()
+        except ValueError:
+            print("number fields must be a number")
+        
+class SaveInventoryMessage(QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+
+        btn = QDialogButtonBox.StandardButton.Ok
+        self.btnBox = QDialogButtonBox(btn)
+        self.btnBox.accepted.connect(self.accept)
+
+        message = QLabel("Inventory saved.")
+
+        layout = QVBoxLayout()
+        layout.addWidget(message)
+        layout.addWidget(self.btnBox)
+
+if __name__ == "main":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
--- main.py
+++ main.py
@@ -14,10 +14,11 @@ import gsheet_update
 
 class Item: 
     Inventory: dict[str, Self] = dict()
-    def __init__(self, name: str, amount: float, cost: float = 0, source: str ="") -> None:
+    def __init__(self, name: str, total_amount: float, current_amount: float = 0, cost: float = 0, source: str ="") -> None:
         self.name: str = name
-        self.amount: float = amount
-        self.availability: bool = True if amount > 0 else False
+        self.current_amount: float = current_amount
+        self.availability: bool = True if current_amount > 0 else False
+        self.total_amount: float = total_amount
         self.cost: float = cost
         self.source: str = source
         Item.Inventory[self.name] = self
@@ -28,36 +29,55 @@ class Item:
     def updateSource(self, newSource: str) -> None:
         self.source = newSource
     def checkOut(self, amount: float) -> None:
-        self.amount -= amount
-        if self.amount == 0: self.availability = False
+        self.current_amount -= amount
+        if self.current_amount == 0: self.availability = False
     def checkIn(self, amount: float) -> None:
-        self.amount =+ amount
-        if self.amount > 0 and self.availability != True: self.availability = True
+        self.current_amount =+ amount
+        if self.current_amount > 0 and self.availability != True: self.availability = True
 
-def createNewItem() -> Item | None:
-    name: str = input("item name: ")
-    if "%s" % (name) not in Item.Inventory:
-        # validate numeric inputs
-        while True:
-            try:
-                amount: float = float(input("Item amount: "))
-                break
-            except ValueError:
-                print("Please enter a number.")
-
-        while True:
-            try:
-                cost: float = float(input("Item cost: "))
-                break
-            except ValueError:
-                print("Please enter a number.")
-
-        source: str = input("Item source: ")
-
-        return Item(name, amount, cost, source)
-    
+def createNewItem(*args) -> Item | None:
+    if args:
+        name = args[0]
+        total_amount = args[1]
+        amount = args[2]
+        cost = args[3]
+        source = args[4]
+        return Item(name, total_amount, amount, cost, source)
     else:
-        print("item already exists!")
+        name: str = input("item name: ")
+        if "%s" % (name) not in Item.Inventory:
+            # validate numeric inputs
+            while True:
+                try:
+                    amount: float = float(input("Item amount: "))
+                    total_equals_current_check = input("Is the total amount the same as the current amount? ([y]/n)").lower()
+                    if total_equals_current_check == "n":
+                        while True:
+                            try:
+                                total_amount: float = float(input("enter the total amount: "))
+                                break
+                            except ValueError:
+                                print("Please enter a number.")
+                    else:
+                        total_amount = amount
+                    break
+                except ValueError:
+                    print("Please enter a number.")
+
+
+            while True:
+                try:
+                    cost: float = float(input("Item cost: "))
+                    break
+                except ValueError:
+                    print("Please enter a number.")
+
+            source: str = input("Item source: ")
+
+            return Item(name, total_amount, amount, cost, source)
+        
+        else:
+            print("item already exists!")
 
 def deleteItem() -> None:
     item_to_delete: str = input("enter name of item to delete: ")
@@ -75,11 +95,11 @@ def checkOutItem() -> None:
     checkout_amount: float = float(input("amount to check out: "))
     if item_to_checkout in Item.Inventory:
         item: Item = Item.Inventory[item_to_checkout] # use "item" as reference to item in inventory
-        if item.amount >= checkout_amount and item.amount > 0:
+        if item.current_amount >= checkout_amount and item.current_amount > 0:
             item.checkOut(checkout_amount)
             print(f"successfully checked out {checkout_amount} of {item_to_checkout}.")
         else:
-            print(f"not enough {item_to_checkout} to check out. there are only {item.amount} left. checkout failed.")
+            print(f"not enough {item_to_checkout} to check out. there are only {item.current_amount} left. checkout failed.")
     else:
         print(f"{item_to_checkout} does not exist. Try viewing the inventory to see all items.")
 
@@ -92,13 +112,13 @@ def checkInItem() -> None:
     else:
         print(f"{item_to_checkin} does not exist. Try viewing the inventory to see all items.")
 
-def unpackInventory() -> pd.DataFrame:
+def unpackInventory(inv: dict) -> pd.DataFrame:
     table: list = list()
-    for key, item in Item.Inventory.items():
-        table.append([key, item.amount, ("yes" if item.availability  else "no"), item.cost, item.source])
+    for key, item in inv.items():
+        table.append([key, item.current_amount, item.total_amount, ("yes" if item.availability  else "no"), item.cost, item.source])
     
     # print(tabulate(table, headers=['Name', 'Amount', 'Available?', 'Cost', 'Source']))
-    return pd.DataFrame(table, columns=["Name", "Amount", "Available?", "Cost", "Source"])
+    return pd.DataFrame(table, columns=["Name", "Current Amount", "Total Amount","Available?", "Cost", "Source"])
 
 def adjustItemAttribute(attribute: str) -> None:
     selected_item: str = input("enter item name: ")
@@ -120,51 +140,52 @@ def saveInventory() -> None:
 def loadInventory():
     with open('Inventory.pkl', 'rb') as f:
         return pickle.load(f)
+    
+if __name__ == "__main__":
+    # load existing on startup
+    if os.path.exists('./Inventory.pkl'):
+        Item.Inventory = loadInventory()
 
-# load existing on startup
-if os.path.exists('./Inventory.pkl'):
-    Item.Inventory = loadInventory()
-
-# CLI loop
-loop = True
-for n in range(50): print("-",sep=' ', end='', flush=True)
-print("\nWelcome to the inventory system.\n")
-while loop:
+    # CLI loop
+    loop = True
     for n in range(50): print("-",sep=' ', end='', flush=True)
-    selection: str = input("\nWhat would you like to do?\n"\
-                        "(N)ew item\n"\
-                        "(D)elete item\n"\
-                        "Check (O)ut items\n"\
-                        "Check (I)n items\n"
-                        "(V)iew inventory\n"\
-                        "Update item (c)ost\n"\
-                        "Update Item (s)ource\n"\
-                        "(E)xit\n"\
-                        ).lower() # get user selection, accept any case
-    if selection == "n" or selection == "new":
-        item = createNewItem() # create new item
-        print(f"{item} created.")
-    elif selection == "d" or selection == "delete":
-        deleteItem() # delete item function
-    elif selection == "o" or selection == "out":
-        checkOutItem() # make call to checkout function
-    elif selection == "i" or selection == "in":
-        checkInItem()
-    elif selection == "v" or selection == "view":
-        print(tabulate(unpackInventory(), headers='keys',showindex=False, tablefmt='pipe', stralign='center', numalign='right')) # type: ignore
-    elif selection == "c" or selection == "cost":
-        adjustItemAttribute("cost") # run function to adjust attributes. 
-    elif selection == "s" or selection == "source":
-        adjustItemAttribute("source")
-    elif selection == "e" or selection == "exit":
-        loop = False
+    print("\nWelcome to the inventory system.\n")
+    while loop:
+        for n in range(50): print("-",sep=' ', end='', flush=True)
+        selection: str = input("\nWhat would you like to do?\n"\
+                            "(N)ew item\n"\
+                            "(D)elete item\n"\
+                            "Check (O)ut items\n"\
+                            "Check (I)n items\n"
+                            "(V)iew inventory\n"\
+                            "Update item (c)ost\n"\
+                            "Update Item (s)ource\n"\
+                            "(E)xit\n"\
+                            ).lower() # get user selection, accept any case
+        if selection == "n" or selection == "new":
+            item = createNewItem() # create new item
+            print(f"{item} created.")
+        elif selection == "d" or selection == "delete":
+            deleteItem() # delete item function
+        elif selection == "o" or selection == "out":
+            checkOutItem() # make call to checkout function
+        elif selection == "i" or selection == "in":
+            checkInItem()
+        elif selection == "v" or selection == "view":
+            print(tabulate(unpackInventory(Item.Inventory), headers='keys',showindex=False, tablefmt='pipe', stralign='center', numalign='right'))  # pyright: ignore[reportArgumentType]
+        elif selection == "c" or selection == "cost":
+            adjustItemAttribute("cost") # run function to adjust attributes. 
+        elif selection == "s" or selection == "source":
+            adjustItemAttribute("source")
+        elif selection == "e" or selection == "exit":
+            loop = False
 
-save:bool = True if input("save inventory? ([y]/n)").lower() != "n" else False
+    save:bool = True if input("save inventory? ([y]/n)").lower() != "n" else False
 
-if save:
-    # do save stuff
-    saveInventory()
-    push:bool = False if input("push updates to google? (y/[n])").lower() != "y" else True
-    if push:
-        inv = unpackInventory()
-        gsheet_update.updateInvSheet(inv)
+    if save:
+        # do save stuff
+        saveInventory()
+        push:bool = False if input("push updates to google? (y/[n])").lower() != "y" else True
+        if push:
+            inv = unpackInventory(Item.Inventory)
+            gsheet_update.updateInvSheet(inv)
